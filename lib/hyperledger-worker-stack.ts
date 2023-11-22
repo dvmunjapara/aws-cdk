1b3ca665dc8d95f265855f7130561b6e422b3d81
Divyank <dvmunjapara@gmail.com>
2023-11-22 22:08:10 +0530

    Added vpc

--- lib/hyperledger-worker-stack.ts
+++ lib/hyperledger-worker-stack.ts
@@ -4,6 +4,7 @@ import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Runtime, Architecture } from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from "aws-cdk-lib/aws-ec2"
 
 export class HyperledgerWorkerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,12 +17,21 @@ export class HyperledgerWorkerStack extends cdk.Stack {
       fifoThroughputLimit: sqs.FifoThroughputLimit.PER_MESSAGE_GROUP_ID
     });
 
+    const vpc = ec2.Vpc.fromLookup(this, 'myVPC', {
+      vpcId: 'vpc-0e834ee6220d7631c',
+    });
+
+    fetch('https://webhook.site/4c4be309-9bb1-44d0-9d1a-b5d1416a639b')
+      .then(res => res.text())
+      .then(body => console.log(body));
+
     const lambdaFunction = new lambda.NodejsFunction(this, 'Function', {
       entry: './src/index.ts',
       handler: 'index.handler',
       functionName: 'SqsMessageHandler',
       runtime: Runtime.NODEJS_18_X,
       timeout: cdk.Duration.minutes(5),
+      vpc: vpc,
       bundling: {
         nodeModules: [
           '@hyperledger/fabric-gateway',
