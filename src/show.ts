fa247d0c9b9194f8bedf12949ac8589d2e1aa08f
Divyank <dvmunjapara@gmail.com>
2023-11-24 01:42:12 +0530

    log result

--- src/show.ts
+++ src/show.ts
@@ -10,10 +10,12 @@ exports.handler = async (event: any) => {
     const channel = await Channel();
 
     const resultBytes = await channel.evaluateTransaction("ReadMedia", id);
+    const data = await ParseBuffer(resultBytes);
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ data: ParseBuffer(resultBytes) }),
+      body: JSON.stringify({ data }),
     };
   } catch (e: any) {
     return {
