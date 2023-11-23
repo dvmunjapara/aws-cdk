2da8635578531f75516fd050f6cb0d56125b73c5
Divyank <dvmunjapara@gmail.com>
2023-11-24 01:36:57 +0530

    log result

--- src/Hyperledger/parse-buffer.ts
+++ src/Hyperledger/parse-buffer.ts
@@ -6,6 +6,7 @@ export async function ParseBuffer(bytes: Uint8Array): Promise<any> {
 
   const resultJson = utf8Decoder.decode(bytes);
 
+  console.log({resultJson})
   try {
     return JSON.parse(resultJson);
   } catch (e) {
