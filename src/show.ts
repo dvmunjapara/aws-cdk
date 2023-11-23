e6f8be7fd6012e6522446cc6fb69ec6b11dea22b
Divyank <dvmunjapara@gmail.com>
2023-11-24 01:32:01 +0530

    Get media

--- src/show.ts
+++ src/show.ts
@@ -1,11 +1,26 @@
+import {Channel, ParseBuffer} from "./Hyperledger";
 
 exports.handler = async (event: any) => {
 
   console.log("request:", JSON.stringify(event, undefined, 2));
 
-  return {
-    statusCode: 200,
-    body: `This page has been viewed!`
-  };
+  const id = event.pathParameters.id;
+
+  try {
+    const channel = await Channel();
+
+    const resultBytes = await channel.evaluateTransaction("ReadMedia", id);
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ data: ParseBuffer(resultBytes) }),
+    };
+  } catch (e: any) {
+    return {
+      statusCode: 404,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ error: "No transaction found" }),
+    };
+  }
 
 };
