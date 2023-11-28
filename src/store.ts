fe330cc1f3d3c33588bf464b874ea7c19e2816e4
Divyank <dvmunjapara@gmail.com>
2023-11-28 20:42:59 +0530

    Updated template

--- src/store.ts
+++ src/store.ts
@@ -17,7 +17,7 @@ exports.handler = async (event: any) => {
         "CreateMedia",
         {
           arguments: [
-            media.id+'',
+            (media.id).toString(),
             JSON.stringify(media),
           ]
         }
