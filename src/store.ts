990a517560a0d4df24844147af83d4be90dc6a6d
Divyank <dvmunjapara@gmail.com>
2023-11-28 21:18:47 +0530

    Updated template

--- src/store.ts
+++ src/store.ts
@@ -4,8 +4,6 @@ exports.handler = async (event: any) => {
 
   for (const record of event.Records) {
 
-    console.log(record.body);
-
     const media = JSON.parse(record.body);
 
     console.log(media)
