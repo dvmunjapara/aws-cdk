bd65d0d2d1a82c8a7eabb18d620d8b91ae4182a6
Divyank <dvmunjapara@gmail.com>
2023-12-18 23:30:57 +0530

    Log errors

--- src/hyperledger/search.ts
+++ src/hyperledger/search.ts
@@ -1,9 +1,18 @@
+import {json} from "node:stream/consumers";
+
 exports.handler = async (event: any) => {
 
   try {
 
     const body = JSON.parse(event.body);
     const nano = require('nano')(process.env.COUCHDB_HOST);
+
+    console.log({
+      keys: body.frames,
+      db: process.env.COUCHDB_DATABASE,
+      host: process.env.COUCHDB_HOST
+    });
+
     const db = nano.use(process.env.COUCHDB_DATABASE);
 
     const docs = await db.view("frames-doc", "frames-view", {
@@ -17,6 +26,8 @@ exports.handler = async (event: any) => {
       body: JSON.stringify({data: docs.rows}),
     }
   } catch (e: any) {
+
+    console.log({e: JSON.stringify(e)});
     return {
       statusCode: 404,
       headers: {"Content-Type": "application/json"},
