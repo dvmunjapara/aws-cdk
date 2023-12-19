54bc7362019d98d0a93c26f09b7cd402bb534604
Divyank <dvmunjapara@gmail.com>
2023-12-19 23:45:52 +0530

    Do singular search for the frame

--- src/hyperledger/search.ts
+++ src/hyperledger/search.ts
@@ -5,7 +5,6 @@ exports.handler = async (event: any) => {
   try {
 
     const body = JSON.parse(event.body);
-    const nano = require('nano')(process.env.COUCHDB_HOST);
 
     console.log({
       keys: body.frames,
@@ -13,18 +12,32 @@ exports.handler = async (event: any) => {
       host: process.env.COUCHDB_HOST
     });
 
-    const db = nano.use(process.env.COUCHDB_DATABASE);
 
-    const docs = await db.view("frames-doc", "frames-view", {
-      keys: body.frames,
-      include_docs: true,
-    });
+    let promiss = [];
+    for (const frame of body.frames) {
+
+      promiss.push(search(frame))
+
+    }
+
+    const docs = await Promise.all(promiss);
+
+    if (docs) {
+
+      return {
+        statusCode: 200,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({data: docs}),
+      }
+    }
+
 
     return {
       statusCode: 200,
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({data: docs.rows}),
+      body: JSON.stringify({data: []}),
     }
+
   } catch (e: any) {
 
     console.log({e: JSON.stringify(e)});
@@ -36,3 +49,15 @@ exports.handler = async (event: any) => {
   }
 
 };
+const search = async function (frame: string) {
+
+  const nano = require('nano')(process.env.COUCHDB_HOST);
+
+  const db = nano.use(process.env.COUCHDB_DATABASE);
+
+  return  await db.view("frames-doc", "frames-view", {
+    key: frame,
+    include_docs: true,
+  });
+
+}
