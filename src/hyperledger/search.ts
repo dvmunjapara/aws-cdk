d886eafd824740b9fa68411d4fa5de976653f90f
Divyank <dvmunjapara@gmail.com>
2023-12-20 01:30:29 +0530

    Optimize transaction search

--- src/hyperledger/search.ts
+++ src/hyperledger/search.ts
@@ -18,7 +18,7 @@ exports.handler = async (event: any) => {
     let found = false;
 
     const frameChunks = body.frames.reduce((resultArray: string[][], item: string, index: number) => {
-      const chunkIndex = Math.floor(index/250)
+      const chunkIndex = Math.floor(index/450)
 
       if(!resultArray[chunkIndex]) {
         resultArray[chunkIndex] = [] // start a new chunk
